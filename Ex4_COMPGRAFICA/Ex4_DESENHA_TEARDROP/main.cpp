c54d7d3980b6c70be93aa4390856e1c3489551f9
Antonio Carlos Barco Tanahashi <82279367+antoniobt12062002@users.noreply.github.com>
2023-04-26 11:56:00 -0300

    Update main.cpp

--- Ex4_COMPGRAFICA/Ex4_DESENHA_TEARDROP/main.cpp
+++ Ex4_COMPGRAFICA/Ex4_DESENHA_TEARDROP/main.cpp
@@ -1,33 +1,57 @@
 #include <GL/glut.h>
-#include <iostream>
-#include <stdio.h>
 #include <math.h>
 
-void display(void)
-{
+int CliqueMouseX, CliqueMouseY; // coordenadas do X, Y
+int raio = 30;
+
+void desenhaCirculo(int x, int y, int raio) {
+    glBegin(GL_LINE_STRIP);
+    double angulo = 0;
+    for (double i = 0; i <= 360; i++) {
+        glVertex2i(x + raio * cos(angulo), y + raio * sin(angulo)); // adiciona um vértice com coordenadas calculadas a partir do ângulo atual
+        angulo = i * (M_PI / 360); // atualiza o ângulo em radianos
+    }
+    glEnd();
+}
+
+void desenhaLinha(int x1, int y1, int x2, int y2) {
+    glBegin(GL_LINES);
+    glVertex2i(x1, y1); // adiciona o primeiro ponto da linha
+    glVertex2i(x2, y2); // adiciona o segundo ponto da linha
+    glEnd();
+}
+
+void desenhaLagrima(int x, int y, int raio) {
+    desenhaLinha(x, y - 90, x - raio, y); // desenha a primeira linha da lágrima
+    desenhaCirculo(x, y, raio); // desenha o círculo da lágrima
+    desenhaLinha(x, y - 90, x + raio, y); // desenha a segunda linha da lágrima
+}
+
+void display(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0, 1.0, 1.0);
 
-    glBegin(GL_LINE_STRIP);
-
-    for (float x = -10; x <= 10; x += 0.1)
-    {
-        float y = sin(x) * cos(x); // função escolhida
-        glVertex2f(50 * x + 250, 50 * y + 250); // mapeando a escala do gráfico para a janela
-    }
-
-    glEnd();
+    desenhaLagrima(CliqueMouseX, CliqueMouseY, raio);
 
     glFlush();
 }
 
-int main(int argc, char** argv)
-{
+void mouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        CliqueMouseX = x;
+        CliqueMouseY = y;
+        glutPostRedisplay();
+    }
+}
+
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(400, 400);
-    glutCreateWindow("y = sin(x) * cos(x)");
+    glutCreateWindow("Tear Drop");
     gluOrtho2D(0, 400, 400, 0);
     glutDisplayFunc(display);
+    glutMouseFunc(mouse);
     glutMainLoop();
+    return 0;
 }
